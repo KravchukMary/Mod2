672be3e5358fc151793826dabedfa1f0d78391c4
Mariya <kravchuk_mary@mail.ru>
2022-09-18 18:47:15 +0300

    Ex002

---
+++ Homework01/Ex002/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Вводим чмсло a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводим число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("Максимальное число " + a);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.WriteLine("Максимальное число " + b);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число " + c);
+    }
+}
