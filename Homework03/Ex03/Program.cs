c8b529932019cd3c65a487276c38d43dce03ddc7
Mariya <kravchuk_mary@mail.ru>
2022-09-26 01:55:55 +0300

    Выполнила домашнее задание

---
+++ Homework03/Ex03/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 23
+
+// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine()??"");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix = value);
+}
+
+string LineNum(int numN, int p)
+{
+    string outLine = "1";
+    for(int i = 2; i <= numN; i++)
+    {
+        outLine = outLine +"\t " + Math.Pow(i, p);
+    } 
+    return outLine;
+}
+
+int numN = ReadData("Введите число N: ");
+
+string lineTop = LineNum (numN, 1);
+string lineDown = LineNum (numN, 3);
+
+PrintData("", lineTop);
+PrintData("", lineDown);
