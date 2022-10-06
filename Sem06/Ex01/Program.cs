5f8cd0f5fcdf983e68b354af04c0eec6c19866c9
Mariya <kravchuk_mary@mail.ru>
2022-10-07 00:11:49 +0300

    Seminar

---
+++ Sem06/Ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 60.
+// Составить частотный словарь элементов двумерного массива.
+
+
+int [] arr = {0, 1, 1, 6, 7, 8, 3, 3, 4, 4, 5, 5, 6, 7, 8, 9, 3, 3, 1, };
+int[] count = new int[arr.Max() + 1];
+
+for(int i = 0; i < arr.Length; i++) count[arr[i]]++;
+
+for(int i = 0; i < count.Length; i++);
+{
+    if (count[i] ≠ 0)
+    {
+    Console.WriteLine(${i} встречается {count[i]}p);
+    }
+}
